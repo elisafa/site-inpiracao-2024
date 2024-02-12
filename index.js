1d05c3e930bae5c67eec748f5c2939cfb3a17158
Elisafa <elisafajunior@hotmail.com>
2024-02-12 13:54:35 -0300

    responsividade 7

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const questionsIcone = document.querySelectorAll(".quest-icone")
+const questionMargin = document.querySelector(".question")
+// const res = document.querySelectorAll(".res")
+
+questionsIcone.forEach((questionIcone) => {
+  questionIcone.addEventListener("click", () => {
+    const resposta = questionIcone.parentElement.nextElementSibling
+    resposta.classList.toggle("show")
+    if (questionIcone.innerHTML === "+") {
+      questionIcone.innerHTML = "-"
+    } else {
+      questionIcone.innerHTML = "+"
+    }
+  })
+})
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
   centeredSlides: false,
